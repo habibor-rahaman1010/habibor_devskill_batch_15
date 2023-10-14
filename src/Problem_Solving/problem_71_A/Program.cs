bed64fe837bf2d524654b2e16a14e765e15f48d6
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-14 18:28:06 +0600

    ok

---
+++ src/Problem_Solving/problem_71_A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WayTooLongWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                if (word.Length > 10)
+                {
+                    string abbreviatedWord = $"{word[0]}{word.Length - 2}{word[word.Length - 1]}";
+                    Console.WriteLine(abbreviatedWord);
+                }
+                else
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
+
