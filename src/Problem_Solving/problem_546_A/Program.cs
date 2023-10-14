587a6b3d541a04be624cbd8aec7362ea1e3161bc
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-14 18:07:07 +0600

    ok

--- src/Problem_Solving/problem_546_A/Program.cs
+++ src/Problem_Solving/problem_546_A/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SoldierAndBananas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int k = int.Parse(input[0]);
+            int n = int.Parse(input[1]);
+            int w = int.Parse(input[2]);
+
+            int totalCost = 0;
+            for (int i = 1; i <= w; i++)
+            {
+                totalCost += i * k;
+            }
+
+            Console.WriteLine(Math.Max(0, totalCost - n));
+        }
+    }
+}
+
