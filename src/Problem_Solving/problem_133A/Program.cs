dacdaef1452aaa2d0721dbcd9a204bcc603cbaa0
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-15 11:28:20 +0600

    ok done problem HQ9+

---
+++ src/Problem_Solving/problem_133A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HQ
+{
+    class Program
+    {
+        static string CheckHQ9p(string program)
+        {
+            foreach (char c in program)
+            {
+                if (c == 'H' || c == 'Q' || c == '9')
+                {
+                    return "YES";
+                }
+            }
+            return "NO";
+        }
+        static void Main(string[] args)
+        {
+            string p = Console.ReadLine().Trim();
+            Console.WriteLine(CheckHQ9p(p));
+        }
+    }
+}
