e90341cccdf1bc21a26eb905610f07ecba508e6f
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-15 12:10:00 +0600

    ok

---
+++ src/Problem_Solving/problem_58A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChatRoom
+{
+    class Program
+    {
+        static bool CheckHello(string s)
+        {
+            string hello = "hello";
+            int index = 0;
+
+            foreach (char c in s)
+            {
+                if (c == hello[index])
+                {
+                    index++;
+                    if (index == hello.Length)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().Trim();
+            Console.WriteLine(CheckHello(s) ? "YES" : "NO");
+        }
+    }
+}
