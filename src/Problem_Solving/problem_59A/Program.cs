7008348a33292da7ab4d5771bb63be90abe63869
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-16 10:56:46 +0600

    ok

---
+++ src/Problem_Solving/problem_59A/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WordCasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int upperCaseCount = 0;
+            int lowerCaseCount = 0;
+
+            foreach (char c in s)
+            {
+                if (Char.IsUpper(c))
+                {
+                    upperCaseCount++;
+                }
+                else
+                {
+                    lowerCaseCount++;
+                }
+            }
+
+            if (upperCaseCount > lowerCaseCount)
+            {
+                Console.WriteLine(s.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(s.ToLower());
+            }
+        }
+    }
+}
