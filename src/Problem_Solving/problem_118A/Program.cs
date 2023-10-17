9b0a56bb5e03bc3f016177a6d5776bf3d0e4e43b
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-17 12:25:00 +0600

    ok

---
+++ src/Problem_Solving/problem_118A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.ToLower();
+
+            string result = "";
+
+            foreach (char c in input)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
+                {
+                    continue;
+                }
+                else
+                {
+                    result += "." + c;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
