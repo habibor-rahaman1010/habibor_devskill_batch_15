3adf5594b092eecf29813131db03558870426cc1
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-30 22:39:27 +0600

    ok

--- src/Exam2/Task3/Program.cs
+++ src/Exam2/Task3/Program.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-Console.WriteLine("Please enter the string: ");
-string input = Console.ReadLine();
-
-string result = ModifyString(input);
-Console.WriteLine(result);
-
-
-static string ModifyString(string input)
+class Program
 {
-    string result = Regex.Replace(input, "[^a-zA-Z0-9_ ]", "");
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter the string: ");
+        string input = Console.ReadLine();
 
-    result = result.Replace('_', ' ');
-    
+        string result = ModifyString(input);
 
-    result = Regex.Replace(result, @"\s+", " ");
+        Console.WriteLine(result);
+    }
 
-    return result.Trim();
+    static string ModifyString(string input)
+    {
+        string withoutSpecialChars = Regex.Replace(input, "[^a-zA-Z0-9_ ]", "");
+        string[] words = withoutSpecialChars.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = CapitalizeFirstLetter(words[i].Replace("_", " "));
+        }
+
+        return string.Join(" ", words);
+    }
+
+    static string CapitalizeFirstLetter(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return string.Empty;
+        }
+
+        return char.ToUpper(word[0]) + word.Substring(1).ToLower();
+    }
 }
