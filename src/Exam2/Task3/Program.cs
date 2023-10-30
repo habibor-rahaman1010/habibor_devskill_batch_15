b57aac1f0ee41383fc5525c66dca2f6407eba0ec
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-30 22:25:12 +0600

    OK

--- src/Exam2/Task3/Program.cs
+++ src/Exam2/Task3/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Please enter the string: ");
+string input = Console.ReadLine();
+
+string result = ModifyString(input);
+Console.WriteLine(result);
+
+
+static string ModifyString(string input)
+{
+    string result = Regex.Replace(input, "[^a-zA-Z0-9_ ]", "");
+
+    result = result.Replace('_', ' ');
+    System.Globalization.CultureInfo cultureInfo = System.Threading.Thread.CurrentThread.CurrentCulture;
+    System.Globalization.TextInfo textInfo = cultureInfo.TextInfo;
+    result = textInfo.ToTitleCase(result.ToLower());
+
+    result = Regex.Replace(result, @"\s+", " ");
+
+    return result.Trim();
+}
