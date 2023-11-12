e4b50945957fd8c01af23f914d10edee608ba4b0
habibor-rahaman1010 <habibor144369@gmail.com>
2023-11-12 21:51:20 +0600

    done 1 allhumdullilah

--- src/Assignment2/Task3/Program.cs
+++ src/Assignment2/Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Programmer");
+        }
+    }
+}
