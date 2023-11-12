9d927720f50a3d657b0fb6fde8757e6f9b5fbc6d
habibor-rahaman1010 <habibor144369@gmail.com>
2023-11-12 22:03:39 +0600

    ok 2nd is done

--- src/Assignment2/Task2/Program.cs
+++ src/Assignment2/Task2/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Task2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Programmer");
+            Console.WriteLine("Enter first large number:");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second large number:");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine(AddLargeNumbers(num1, num2));
+        }
+        public static string AddLargeNumbers(string num1, string num2)
+        {
+            int maxLength = Math.Max(num1.Length, num2.Length);
+            num1 = num1.PadLeft(maxLength, '0');
+            num2 = num2.PadLeft(maxLength, '0');
+
+            char[] result = new char[maxLength + 1];
+            int carry = 0;
+
+            for (int i = maxLength - 1; i >= 0; i--)
+            {
+                int digit1 = num1[i] - '0';
+                int digit2 = num2[i] - '0';
+
+                int sum = digit1 + digit2 + carry;
+                carry = sum / 10;
+                result[i + 1] = (char)((sum % 10) + '0');
+            }
+
+            result[0] = (char)(carry + '0');
+            string sumString = new string(result);
+            sumString = sumString.TrimStart('0');
+            return sumString;
         }
     }
 }
