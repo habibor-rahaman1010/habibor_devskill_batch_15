8cce1fd25f47d3192ea3344647d9e83b10ede784
habibor-rahaman1010 <habibor144369@gmail.com>
2023-11-23 21:55:55 +0600

    ok

--- src/Exam3/Task3/Program.cs
+++ src/Exam3/Task3/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Task3;
+
+int randomNumber = RandomNumberGenerator.Generate();
+Console.WriteLine($"Generated random number: {randomNumber}");
