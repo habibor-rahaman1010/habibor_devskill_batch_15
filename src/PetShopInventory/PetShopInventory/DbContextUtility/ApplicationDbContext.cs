df23400ecac091e2e93c78d20d9cb8b35b56e168
habibor-rahaman1010 <habibor144369@gmail.com>
2023-12-16 11:07:44 +0600

    ok

---
+++ src/PetShopInventory/PetShopInventory/DbContextUtility/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShopInventory.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopInventory
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ApplicationDbContext()
+        {
+            _connectionString = "Server=HABIBOR-RAHAMAN\\SQLEXPRESS;Database=PetShopInventoryDB;User Id=habibor144369;Password=c++c++c#;Trust Server Certificate=True;";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(GetUser().ToList());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private List<User> GetUser()
+        {
+            return new List<User> { new User { Id = 1, Name = "admin", Password = "123456" } };
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ src/PetShopInventory/PetShopInventory/Program.cs
@@ -0,0 +1,60 @@
+﻿using PetShopInventory.Account;
+using System;
+using System.Threading.Channels;
+
+namespace PetShopInventory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            User? user =  context.Users.FirstOrDefault();
+
+            Console.WriteLine("Plese enter your user name: ");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine("Plese enter your password");
+            string? password = Console.ReadLine();
+
+            if (user != null)
+            {
+                if (user.Name == name && user.Password == password)
+                {
+                    Console.WriteLine("\n--------User login successfully--------- \n");
+                    Console.WriteLine("""
+                        Inpute 1: to Change Password: 
+                        Inpute 2: Go To Pet Shop Inventory: 
+                         
+                        """);
+                    int condition = int.Parse(Console.ReadLine());
+                    switch (condition)
+                    {
+                        case 1:
+                            Console.WriteLine("--Please give new credeincial for update user name and password---");
+                            Console.WriteLine("Enter New User Name: ");
+                            string newuserName = Console.ReadLine();
+                            Console.WriteLine("Enter New User Name: ");
+                            string newPassword = Console.ReadLine();
+                            user.Name = newuserName;
+                            user.Password = newPassword;
+                            context.SaveChanges();
+                            break;
+
+                        case 2:
+                            Console.WriteLine($"---{user.Name} Hello, You Can Operate In This Below Options----");
+                            break;
+
+                        default:
+                            Console.WriteLine("Don't match");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n--------user name or password don't match, plese tryagain--------\n");
+                }
+            }
+        }
+    }
+}
